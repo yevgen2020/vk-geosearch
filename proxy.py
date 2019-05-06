3761aaa4c67402eb6a5afe0dedc9e30373fa7b3a
yevgen2020 <47853647+yevgen2020@users.noreply.github.com>
2019-05-06 18:37:31 +0300

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -11,12 +11,10 @@ loop = asyncio.get_event_loop()
 types = ['HTTPS']
 codes = [200, 301, 302]
 
-countries = ['FR', 'DE', 'CA', 'NL', 'US']
-
 broker = Broker(max_tries=1, loop=loop)
 
 broker.serve(host=host, port=port, types=types, limit=10, max_tries=1,
-             min_req_proxy=5, max_error_rate=0.5, max_resp_time=8, countries=countries, backlog=100)
+             min_req_proxy=5, max_error_rate=0.5, max_resp_time=8, backlog=100)
 
 loop.run_forever()
 
